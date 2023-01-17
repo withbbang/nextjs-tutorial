fb016288745f68ea9c8fb7747870e07e95d5648b
김영선 <gim-yeongseon@gim-yeongseon-ui-MacBookPro.local>
2023-01-17 22:58:49 +0900

    환경변수 추가 방법, 실제 요청 url 마스킹, redirect 방법 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
+/**
+ * 이 파일은 수정할 때 마다 서버 재기동을 해야 적용된다.
+ */
+
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/before', //리다이렉트 트리거 url
+        destination: '/after', //리다이렉트 목적 url
+        permanent: false //해당 리다이렉션이 영구/비영구인지에 따라 브라우저나 검색엔진이 해당 정보 기억함
+      },
+      {
+        source: '/before/:param', //목적 url에 파라미터 그대로 전달
+        destination: '/after/:param',
+        permanent: false
+      },
+      {
+        source: '/before/:param*', //목적 url에 후문 모든 경로 전달
+        destination: '/after/:param*',
+        permanent: false
+      }
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api', //네트워크 콘솔에 보일 url
+        destination: 'https://api/themoviedb.org/3/movie/blabla' //실제 요청 url
+      },
+      {
+        source: '/api/popular-movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      },
+      {
+        source: '/api/popular-movies/:language/:page',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}${encodeURIComponent(
+          '&'
+        )}language=:language${encodeURIComponent('&')}page=:page` // url에 쿼리스트링이 들어갈 경우
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
